830dd421f3da0e6d69cb1fccdeb715d6abd08be5
Chris Cannam <cannam@all-day-breakfast.com>
2017-04-26 15:22:28 +0100

    Binary search for data element, + describe

--- src/shapes/line.js
+++ src/shapes/line.js
@@ -1,6 +1,5 @@
 import BaseShape from './base-shape';
 
-
 /**
  * A shape to display a line. Its main use is as common shape to create a
  * breakpoint visualization. (entity shape)
@@ -26,36 +25,70 @@ export default class Line extends BaseShape {
     return this.$el;
   }
 
-  update(renderingContext, data) {
-    
-    const before = performance.now();
+  encache(data) {
 
     data = data.slice(0);
     data.sort((a, b) => this.cx(a) < this.cx(b) ? -1 : 1);
 
+    return data;
+  }
+
+  _findInData(data, x) {
+
+    // Binary search, demands that data has been encached
+    // (i.e. sorted). Returns index of value that matches x, or the
+    // index just before where it would be if there is no exact
+    // match. (This means the returned index is always in range for
+    // the input array unless the array is empty.)  Note that x must
+    // be in model coordinates (e.g. time), not pixel coords.
+
+    let low = 0;
+    let high = data.length - 1;
+
+    while (low <= high) {
+      let mid = low + ((high - low) >> 1);
+      let value = this.cx(data[mid]);
+      if (value < x) {
+        low = mid + 1;
+      } else if (value > x) {
+        high = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+    
+    return high;
+  }
+  
+  update(renderingContext, data) { // data array is sorted already
+    
+    const before = performance.now();
+
     const minX = Math.floor(renderingContext.minX);
     const maxX = Math.ceil(renderingContext.maxX);
 
+    console.log("minX = " + minX + ", maxX = " + maxX);
+    
     let instructions = [];
     const n = data.length;
-
+    
     if (n > 0) {
 
       // We want to start with the last element to the left of the
       // visible region, and end with the first element beyond the
       // right of it
+
+      let cx0 = renderingContext.timeToPixel.invert(minX);
+      let i0 = this._findInData(data, cx0);
       
-      let nextX = renderingContext.timeToPixel(this.cx(data[0]));
+      let nextX = renderingContext.timeToPixel(this.cx(data[i0]));
     
-      for (let i = 0; i < n; ++i) {
+      for (let i = i0; i < n; ++i) {
 
         const x = nextX;
 
         if (i + 1 < n) {
           nextX = renderingContext.timeToPixel(this.cx(data[i+1]));
-        }
-        if (nextX < minX) {
-          continue;
         }
 
         const y = renderingContext.valueToPixel(this.cy(data[i])) - 0.5;
@@ -94,5 +127,12 @@ export default class Line extends BaseShape {
     });
 
     return 'M' + instructions.join('L');
+  }
+
+  describe(renderingContext, data, x) {
+    if (!data.length) return 0;
+    let cx = renderingContext.timeToPixel.invert(x);
+    let i = this._findInData(data, cx);
+    return data[i];
   }
 }
