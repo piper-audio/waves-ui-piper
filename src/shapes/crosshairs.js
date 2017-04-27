6b2afcb9beeb1c46e3abf4bf768acc55f29d3e6e
Chris Cannam <cannam@all-day-breakfast.com>
2017-04-27 16:11:00 +0100

    Add value labels to highlight crosshairs

--- src/shapes/crosshairs.js
+++ src/shapes/crosshairs.js
@@ -12,7 +12,7 @@ export default class Crosshairs extends BaseShape {
   getClassName() { return 'crosshairs'; }
 
   _getAccessorList() {
-    return { cx: 0, cy: 0 };
+    return { cx: 0, cy: 0, value: 0 };
   }
 
   _getDefaults() {
@@ -24,13 +24,41 @@ export default class Crosshairs extends BaseShape {
 
   render(renderingContext) {
     if (this.$el) { return this.$el; }
-    this.$el = document.createElementNS(ns, 'path');
-    this.$el.setAttributeNS(null, 'shape-rendering', 'crispEdges');
-    this.$el.style.stroke = this.params.color;
+
+    this.$el = document.createElementNS(this.ns, 'g');
+
+    this.$path = document.createElementNS(ns, 'path');
+    this.$path.setAttributeNS(null, 'shape-rendering', 'geometricPrecision');
+    this.$path.setAttributeNS(null, 'stroke-width', '1.5');
+    this.$path.style.opacity = this.params.opacity;
+    this.$path.style.stroke = this.params.color;
+    this.$el.appendChild(this.$path);
+
+    this.$labels = [
+      document.createElementNS(this.ns, 'text'),
+      document.createElementNS(this.ns, 'text')
+    ];
+
+    for (let i = 0; i < this.$labels.length; ++i) {
+      const $label = this.$labels[i];
+      $label.classList.add('label');
+      $label.style.fontSize = '10px';
+      $label.style.lineHeight = '10px';
+      $label.style.fontFamily = 'monospace';
+      $label.style.color = '#676767';
+      $label.style.opacity = 0.9;
+      $label.style.mozUserSelect = 'none';
+      $label.style.webkitUserSelect = 'none';
+      $label.style.userSelect = 'none';
+      this.$el.appendChild($label);
+    }
+
     return this.$el;
   }
 
   update(renderingContext, datum) {
+
+    console.log("crosshairs update: datum = " + datum);
     
     const cx = this.cx(datum);
     const cy = this.cy(datum);
@@ -41,8 +69,40 @@ export default class Crosshairs extends BaseShape {
     const maxX = Math.ceil(renderingContext.maxX);
     const h = renderingContext.height;
     
-    this.$el.setAttributeNS(null, 'd',
-                            `M${x},${0}L${x},${h}M${minX},${y}L${maxX},${y}`);
+    this.$path.setAttributeNS(null, 'd',
+                              `M${x},${0}L${x},${h}M${minX},${y}L${maxX},${y}`);
+
+    const value = this.value(datum);
+    const label = value.toPrecision(4);
+    const lw = label.length * 10;
+
+    for (let i = 0; i < this.$labels.length; ++i) {
+
+      const $label = this.$labels[i];
+
+      while ($label.firstChild) {
+        $label.removeChild($label.firstChild);
+      }
+      
+      const $textLeft = document.createTextNode(label);
+      $label.appendChild($textLeft);
+      
+      let lx = minX + 2;
+      if (i == 1) {
+        lx = maxX - lw - 2;
+      }
+      
+      $label.setAttributeNS(
+        null, 'transform', `matrix(1, 0, 0, -1, ${lx}, ${h})`
+      );
+
+      let ly = h - y - 5;
+      if (ly < 10) {
+        ly = h - y + 15;
+      }
+      
+      $label.setAttributeNS(null, 'y', ly);
+    }
   }
 
   /**
